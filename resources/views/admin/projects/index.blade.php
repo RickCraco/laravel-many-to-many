90218d974aeb0ccf9c5829176a1729b50ea80653
RickCraco <riccardocracolici43@gmail.com>
2024-01-17 18:47:33 +0100

    fix

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -10,16 +10,6 @@
             <div class="alert alert-success mt-4">{{ session()->get('message') }}</div>
         @endif
 
-        <ul class="list-group my-3">
-            <li class="list-group-item d-flex align-items-center justify-content-between bg-dark text-white">
-                <h3>Title</h3>
-                <div class="d-flex">
-                    <span class="mx-2 btn btn-primary">Show</span>
-                    <span class="mx-2 btn btn-secondary">Edit</span>
-                    <span class="mx-2 btn btn-danger">Delete</span>
-                </div>
-            </li>
-        </ul>
         <form action="{{ route('admin.projects.index') }}" method="GET">
             <div>
                 <select name="technologies" id="technologies" class="form-select mb-3 w-25">
@@ -33,7 +23,17 @@
                 <button type="submit" class="btn btn-primary">Filter</button>
             </div>
         </form>
-        
+
+        <ul class="list-group my-3">
+            <li class="list-group-item d-flex align-items-center justify-content-between bg-dark text-white">
+                <h3>Title</h3>
+                <div class="d-flex">
+                    <span class="mx-2 btn btn-primary">Show</span>
+                    <span class="mx-2 btn btn-secondary">Edit</span>
+                    <span class="mx-2 btn btn-danger">Delete</span>
+                </div>
+            </li>
+        </ul>
 
         <ul class="list-group">
             @foreach($projects as $project)
